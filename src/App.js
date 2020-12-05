ecae6bae56bc6d46c505aa08cdc8282f20da8bdd
islemsaada <59849896+islemsaada@users.noreply.github.com>
2020-12-05 20:21:11 +0100

    change v1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1> Hello react</h1>
-    <p>Welcome to our demo</p>
+    <h1> Hello React</h1>
+    <p>Welcome to our demo ...</p>
     </div>
   );
 }
